2913b09b25e6671332947845cdf4e6260b8c0013
zaldzod <chizalumecheta@hotmail.com>
2022-09-08 10:29:51 -0700

    betty linted files 4, 5, 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-
 /**
-
  *main - entry point
-
  *
-
  * Return: 0 after printing the function
-
  */
-
 int main(void)
-  
 {puts("\"Programming is like building a multilingual puzzle");
-  
-  return (0);
-  
+return (0);
 }
