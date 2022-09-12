e5ccde3e8d33fe64846895b6e38c77c6f4c043dc
zaldzod <chizalumecheta@hotmail.com>
2022-09-12 10:11:35 -0700

    c99 compatible

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 /**
 *main - Entry point
 *
 *Return: Always 0 (Success/correct)
 */
 int main(void)
-{
-for (int x = 'Z'; x >= 'A'; x--)
+{int x;
+for (x = 'Z'; x >= 'A'; x--)
 {
 int lower_x = tolower(x);
 putchar(lower_x);
