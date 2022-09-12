dd82505666903c7d5ba7f1cf9d1e586338ef9011
zaldzod <chizalumecheta@hotmail.com>
2022-09-12 10:16:15 -0700

    c99 compatible

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 /**
 *main - Entry point
 *
 *Return: Always 0 (Success/correct)
 */
 int main(void)
-{
-for (int x = '0'; x <= '9'; x++)
+{int x;
+int y;
+for (x = '0'; x <= '9'; x++)
 {
 putchar(x);
 }
-for (int x = 'A'; x <= 'F'; x++)
+for (y = 'A'; y <= 'F'; y++)
 {
-int lower_x = tolower(x);
-putchar(lower_x);
+int lower_y = tolower(y);
+putchar(lower_y);
 }
 putchar('\n');
 return (0);
