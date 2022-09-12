20866a66a5ba3ef15bf805df9923153fc56ba9b2
zaldzod <chizalumecheta@hotmail.com>
2022-09-12 10:07:11 -0700

    c99 compatible

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 /**
 *main - Entry point
 *
 *Return: Always 0 (Success/correct)
 */
 int main(void)
-{
+{int x;
 for (int x = '0'; x <= '9'; x++)
 {
 putchar(x);
