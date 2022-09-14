3ef16b01cef00181a692d15ee3040ac5d49f494e
zaldzod <chizalumecheta@hotmail.com>
2022-09-13 21:58:17 -0700

    go

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-* print_sign - return 0 letter not lowercase, 1 letter lowercase**@x: the int to print *
+* print_sign - return 0 letter not lowercase, 1 letter lowercase*
+*@x: the int to print *
 * Return: Always 0.
 */
 int print_sign(int x)
