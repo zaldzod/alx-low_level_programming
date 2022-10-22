ff53b24554a8cffd87d893f722e2276a4b659b6b
zaldzod <chizalumecheta@hotmail.com>
2022-10-21 21:40:31 -0700

    fixed betty compliance

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,7 +14,8 @@ new = malloc(sizeof(list_t));
 if (new == NULL)
 return (NULL);
 new->str = strdup(str);
-for (nchar = 0; str[nchar]; nchar++);
+for (nchar = 0; str[nchar]; nchar++)
+;
 new->len = nchar;
 new->next = *head;
 *head = new;
